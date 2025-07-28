7749bb9c04a1759217ca03c9b4df6c47f7eecc09
Ganesh Bala <96352437+ganeshbala2k3@users.noreply.github.com>
2025-07-28 16:16:39 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ import java.util.*;
 public class Hello{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
         System.out.println("Hello World");
         
     }
